bc871f6544d02d094bdf02bc49ed5893c5f0589e
Remi Rampin <remi@rampin.org>
2026-02-19 21:47:18 -0500

    Use a separate repository per namespace

--- ceph_backup/backup.py
+++ ceph_backup/backup.py
@@ -471,7 +471,8 @@ def backup_rbd_fs(api, vol, now, max_backup_duration):
 
     # Create a job to do the backup
     script = (
-        'printf -- \'backing up filesystem %s/%s \\n\' ' + vol['namespace'] + ' ' + vol['name'] + ' >&2\n'
+        'export RESTIC_REPOSITORY=$RESTIC_REPOSITORY_BASE/' + vol['namespace'] + '\n'
+        + 'printf -- \'backing up filesystem %s/%s \\n\' ' + vol['namespace'] + ' ' + vol['name'] + ' >&2\n'
         + 'if restic init >/dev/null 2>&1; then printf -- \'created new repository\\n\' >&2; fi\n'
         + 'stdbuf -o L -e L'
         + ' restic'
@@ -502,8 +503,8 @@ def backup_rbd_fs(api, vol, now, max_backup_duration):
                                 image_pull_policy=BACKUP_IMAGE_PULL_POLICY,
                                 args=['sh', '-c', script],
                                 env=format_env(
-                                    RESTIC_REPOSITORY=(
-                                        'secret', RESTIC_SECRET_NAME, 'url',
+                                    RESTIC_REPOSITORY_BASE=(
+                                        'secret', RESTIC_SECRET_NAME, 'base-url',
                                     ),
                                     RESTIC_HOST='rbd-fs-%s-nspvc-%s' % (
                                         vol['namespace'],
@@ -651,7 +652,8 @@ def backup_rbd_block(api, vol, now, max_backup_duration):
 
     # Create a job to do the backup
     script = (
-        'printf -- \'backing up block %s/%s \\n\' ' + vol['namespace'] + ' ' + vol['name'] + ' >&2\n'
+        'export RESTIC_REPOSITORY=$RESTIC_REPOSITORY_BASE/' + vol['namespace'] + '\n'
+        + 'printf -- \'backing up block %s/%s \\n\' ' + vol['namespace'] + ' ' + vol['name'] + ' >&2\n'
         + 'if restic init >/dev/null 2>&1; then printf -- \'created new repository\\n\' >&2; fi\n'
         + 'rbd diff --whole-object --format=json ' + rbd_fq_image
         + ' > /tmp/layout.json'
@@ -683,8 +685,8 @@ def backup_rbd_block(api, vol, now, max_backup_duration):
                                 image_pull_policy=BACKUP_IMAGE_PULL_POLICY,
                                 args=['sh', '-c', script],
                                 env=format_env(
-                                    RESTIC_REPOSITORY=(
-                                        'secret', RESTIC_SECRET_NAME, 'url',
+                                    RESTIC_REPOSITORY_BASE=(
+                                        'secret', RESTIC_SECRET_NAME, 'base-url',
                                     ),
                                     RESTIC_HOST='rbd-block-%s-nspvc-%s' % (
                                         vol['namespace'],
